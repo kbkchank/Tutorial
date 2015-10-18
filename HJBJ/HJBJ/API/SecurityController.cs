d7b4596e81b6ca7db3b96f19dcf81a1ce94d8eb1
kbkchank <kbkchank@gmail.com>
2015-10-18 16:05:42 +0800

    HJBJ 1
    fist commit

---
+++ HJBJ/HJBJ/API/SecurityController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HJBJ.API
+{
+    public class SecurityController : ApiController
+    {
+        //// GET api/security
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/security/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/security
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/security/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/security/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ HJBJ/HJBJ/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HJBJ.MvcApplication" Language="C#" %>
